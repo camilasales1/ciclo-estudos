694116de57f5569b6fc761a36a98214cc3d92b69
Camila Sales <camilasales00@gmail.com>
2023-05-04 21:17:54 -0300

    calculos

--- Ciclo de studos/InputView/DotIndicatorView.swift
+++ Ciclo de studos/InputView/DotIndicatorView.swift
@@ -24,16 +24,17 @@ struct DotIndicator: View {
             Circle()
                 .scaleEffect(
                     slectedPage == pageIndex
-                    ? maxScale
-                    : minScale
-                )
+                        ? maxScale
+                        : minScale
+            )
                 .animation(.spring())
                 .foregroundColor(
                     slectedPage == pageIndex
-                    ? Color.black
-                    : Color.black.opacity(minOpacity)
-                )
+                        ? Color.black
+                        : Color.black.opacity(minOpacity)
+            )
         }
+        
     }
 }
 
--- Ciclo de studos/InputView/InputViewModel.swift
+++ Ciclo de studos/InputView/InputViewModel.swift
@@ -6,26 +6,3 @@
 //
 
 import Foundation
-
-class InputViewModel:ObservableObject {
-    
-    @Published var subject:Subject = Subject(name: "",
-                                              weight: Weight.one.rawValue,
-                                              dificult: Dificulty.easy.rawValue,
-                                              totalQuestions: 0)
-    let name: String = ""
-    @Published var testDate: Date = Date()
-    @Published var studyStartDate: Date = Date()
-    @Published var subjects: [Subject] = [DummyData.fisica,DummyData.portugues]
-    @Published var TotalHours:Date?
-    @Published var subjectName:String = ""
-    @Published var numberOfQuestions:Int = 0
-    @Published var dificulty = Dificulty.easy
-    @Published var weight = Weight.one
-    @Published var remainingTime:Int?
-    @Published var studyDailyTime:Double = 0.0
-    
-    func addSubject(subject:Subject){
-        subjects.append(subject)
-    }
-}
